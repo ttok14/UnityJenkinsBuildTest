0f090a53f59b418bea93a19463a92ea30d5801c7
ttok14 <ttok14@naver.com>
2022-08-30 23:22:15 +0900

    FF22

--- Assets/Editor/TestSceneView.cs
+++ Assets/Editor/TestSceneView.cs
@@ -1,242 +1,242 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
-using System;
-using UnityEditor.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
+//using System;
+//using UnityEditor.SceneManagement;
 
-[CustomEditor(typeof(ZTestClass))]
-public class TestSceneView : Editor
-{
-    public GUIContent New;
-    public GUIContent Save;
-    //public GUIContent InspectInfo;
-    //public GUIContent ToolSetting;
-    //public GUIContent DisplayWindow;
+//[CustomEditor(typeof(ZTestClass))]
+//public class TestSceneView : Editor
+//{
+//    public GUIContent New;
+//    public GUIContent Save;
+//    //public GUIContent InspectInfo;
+//    //public GUIContent ToolSetting;
+//    //public GUIContent DisplayWindow;
 
-    public class DataSub
-    {
-        public ZTestClass.Data src;
-        public SerializedProperty prop;
-        public bool folded;
+//    public class DataSub
+//    {
+//        public ZTestClass.Data src;
+//        public SerializedProperty prop;
+//        public bool folded;
 
-        public DataSub(ZTestClass.Data src)
-        {
-            this.src = src;
-        }
-    }
+//        public DataSub(ZTestClass.Data src)
+//        {
+//            this.src = src;
+//        }
+//    }
 
-    static string LastWindowPosXKey = "TFA_LastWindowPosX";
-    static string LastWindowPosYKey = "TFA_LastWindowPosY";
+//    static string LastWindowPosXKey = "TFA_LastWindowPosX";
+//    static string LastWindowPosYKey = "TFA_LastWindowPosY";
 
-    static Vector2 FixedSize = new Vector2(300, 350);
+//    static Vector2 FixedSize = new Vector2(300, 350);
 
-    ZTestClass Target;
+//    ZTestClass Target;
 
-    Rect rtWindow;
+//    Rect rtWindow;
 
-    public List<DataSub> allData = new List<DataSub>();
+//    public List<DataSub> allData = new List<DataSub>();
 
-    Texture2D captureTex;
-    GUIStyle windowStyle;
-    bool doCapture = true;
+//    Texture2D captureTex;
+//    GUIStyle windowStyle;
+//    bool doCapture = true;
 
-    Editor defaultEditor;
+//    Editor defaultEditor;
 
-    SerializedObject myObj;
-    SerializedProperty prop;
+//    SerializedObject myObj;
+//    SerializedProperty prop;
 
-    bool isDirty = false;
+//    bool isDirty = false;
 
-    Vector2 scrollPos;
+//    Vector2 scrollPos;
 
-    void Init()
-    {
-        New = EditorGUIUtility.TrTextContentWithIcon("NEW", "Grid 새로 생성", "Grid Icon");
-        Save = EditorGUIUtility.TrTextContentWithIcon("저장", "원하는 형태로 저장가능", "SaveAs");
+//    void Init()
+//    {
+//        New = EditorGUIUtility.TrTextContentWithIcon("NEW", "Grid 새로 생성", "Grid Icon");
+//        Save = EditorGUIUtility.TrTextContentWithIcon("저장", "원하는 형태로 저장가능", "SaveAs");
 
-        myObj = new SerializedObject(target);
-        prop = myObj.FindProperty($"{nameof(ZTestClass.allData)}");
-    }
+//        myObj = new SerializedObject(target);
+//        prop = myObj.FindProperty($"{nameof(ZTestClass.allData)}");
+//    }
 
-    private void OnEnable()
-    {
-        Init();
+//    private void OnEnable()
+//    {
+//        Init();
 
-        Target = base.target as ZTestClass;
+//        Target = base.target as ZTestClass;
 
-        if (EditorPrefs.HasKey(LastWindowPosXKey))
-        {
-            rtWindow.x = EditorPrefs.GetFloat(LastWindowPosXKey);
-            rtWindow.y = EditorPrefs.GetFloat(LastWindowPosYKey);
-            rtWindow.width = FixedSize.x;
-            rtWindow.height = FixedSize.y;
-        }
-        else
-        {
-            rtWindow = new Rect(20, 20, FixedSize.x, FixedSize.y);
-        }
+//        if (EditorPrefs.HasKey(LastWindowPosXKey))
+//        {
+//            rtWindow.x = EditorPrefs.GetFloat(LastWindowPosXKey);
+//            rtWindow.y = EditorPrefs.GetFloat(LastWindowPosYKey);
+//            rtWindow.width = FixedSize.x;
+//            rtWindow.height = FixedSize.y;
+//        }
+//        else
+//        {
+//            rtWindow = new Rect(20, 20, FixedSize.x, FixedSize.y);
+//        }
 
-        ResetData();
+//        ResetData();
 
-        doCapture = true;
-    }
+//        doCapture = true;
+//    }
 
-    void ResetData()
-    {
-        allData.Clear();
+//    void ResetData()
+//    {
+//        allData.Clear();
 
-        allData.Capacity = Target.allData.Count;
-        for (int i = 0; i < Target.allData.Count; i++)
-        {
-            allData.Add(new DataSub(Target.allData[i]));
-        }
-    }
+//        allData.Capacity = Target.allData.Count;
+//        for (int i = 0; i < Target.allData.Count; i++)
+//        {
+//            allData.Add(new DataSub(Target.allData[i]));
+//        }
+//    }
 
-    private void OnDisable()
-    {
-        EditorPrefs.SetFloat(LastWindowPosXKey, rtWindow.x);
-        EditorPrefs.SetFloat(LastWindowPosYKey, rtWindow.y);
-    }
+//    private void OnDisable()
+//    {
+//        EditorPrefs.SetFloat(LastWindowPosXKey, rtWindow.x);
+//        EditorPrefs.SetFloat(LastWindowPosYKey, rtWindow.y);
+//    }
 
-    public override void OnInspectorGUI()
-    {
-        EditorGUI.BeginChangeCheck();
-        {
-            base.OnInspectorGUI();
-        }
-        if (EditorGUI.EndChangeCheck())
-        {
-            ResetData();
-        }
-    }
+//    public override void OnInspectorGUI()
+//    {
+//        EditorGUI.BeginChangeCheck();
+//        {
+//            base.OnInspectorGUI();
+//        }
+//        if (EditorGUI.EndChangeCheck())
+//        {
+//            ResetData();
+//        }
+//    }
 
-    private void OnSceneGUI()
-    {
-        if (PrefabStageUtility.GetCurrentPrefabStage() == null)
-        {
-            return;
-        }
+//    private void OnSceneGUI()
+//    {
+//        if (PrefabStageUtility.GetCurrentPrefabStage() == null)
+//        {
+//            return;
+//        }
 
-        if (doCapture)
-        {
-            doCapture = false;
-            windowStyle = new GUIStyle(GUI.skin.window);
-            windowStyle.normal.textColor = Color.white;
-            windowStyle.onNormal = new GUIStyleState() { background = GUI.skin.window.normal.background, textColor = Color.white };
-            windowStyle.fontSize = 15;
-            windowStyle.fontStyle = FontStyle.Bold;
-        }
+//        if (doCapture)
+//        {
+//            doCapture = false;
+//            windowStyle = new GUIStyle(GUI.skin.window);
+//            windowStyle.normal.textColor = Color.white;
+//            windowStyle.onNormal = new GUIStyleState() { background = GUI.skin.window.normal.background, textColor = Color.white };
+//            windowStyle.fontSize = 15;
+//            windowStyle.fontStyle = FontStyle.Bold;
+//        }
 
-        if (Target == null)
-        {
-            return;
-        }
+//        if (Target == null)
+//        {
+//            return;
+//        }
 
-        Handles.BeginGUI();
-        {
-            if (Event.current.type != EventType.Repaint)
-            {
-                int controlId = GUIUtility.GetControlID(FocusType.Passive);
-                rtWindow = GUILayout.Window(controlId, rtWindow, OnDraw, "Fx 정보", windowStyle, GUILayout.Width(FixedSize.x), GUILayout.Height(FixedSize.y));
-            }
-        }
-        Handles.EndGUI();
-    }
+//        Handles.BeginGUI();
+//        {
+//            if (Event.current.type != EventType.Repaint)
+//            {
+//                int controlId = GUIUtility.GetControlID(FocusType.Passive);
+//                rtWindow = GUILayout.Window(controlId, rtWindow, OnDraw, "Fx 정보", windowStyle, GUILayout.Width(FixedSize.x), GUILayout.Height(FixedSize.y));
+//            }
+//        }
+//        Handles.EndGUI();
+//    }
 
-    private void OnDraw(int windowId)
-    {
-        GUILayout.Space(5);
-        DrawToolBar();
-        GUILayout.Space(5);
+//    private void OnDraw(int windowId)
+//    {
+//        GUILayout.Space(5);
+//        DrawToolBar();
+//        GUILayout.Space(5);
 
-        //GUILayout.BeginHorizontal();
-        //GUILayout.FlexibleSpace();
-        //GUILayout.EndHorizontal();
+//        //GUILayout.BeginHorizontal();
+//        //GUILayout.FlexibleSpace();
+//        //GUILayout.EndHorizontal();
 
-        scrollPos = GUILayout.BeginScrollView(scrollPos);
-        {
-            for (int i = 0; i < allData.Count; i++)
-            {
-                DrawElement(allData[i], i);
-            }
-        }
-        GUILayout.EndScrollView();
+//        scrollPos = GUILayout.BeginScrollView(scrollPos);
+//        {
+//            for (int i = 0; i < allData.Count; i++)
+//            {
+//                DrawElement(allData[i], i);
+//            }
+//        }
+//        GUILayout.EndScrollView();
 
-        var sceneView = SceneView.lastActiveSceneView;
-        GUI.DragWindow(new Rect(0, 0, sceneView.position.width, sceneView.position.height));
-    }
+//        var sceneView = SceneView.lastActiveSceneView;
+//        GUI.DragWindow(new Rect(0, 0, sceneView.position.width, sceneView.position.height));
+//    }
 
-    void DrawElement(DataSub data, int idx)
-    {
-        GUILayout.BeginVertical();
-        {
-            data.folded = EditorGUILayout.Foldout(data.folded, new GUIContent(data.src.name), true);
+//    void DrawElement(DataSub data, int idx)
+//    {
+//        GUILayout.BeginVertical();
+//        {
+//            data.folded = EditorGUILayout.Foldout(data.folded, new GUIContent(data.src.name), true);
 
-            if (data.folded)
-            {
-                EditorGUI.indentLevel++;
-                {
-                    EditorGUI.BeginChangeCheck();
-                    {
-                        EditorGUILayout.PropertyField(this.prop.GetArrayElementAtIndex(idx));
-                    }
-                    if (EditorGUI.EndChangeCheck())
-                    {
-                        isDirty = true;
-                    }
+//            if (data.folded)
+//            {
+//                EditorGUI.indentLevel++;
+//                {
+//                    EditorGUI.BeginChangeCheck();
+//                    {
+//                        EditorGUILayout.PropertyField(this.prop.GetArrayElementAtIndex(idx));
+//                    }
+//                    if (EditorGUI.EndChangeCheck())
+//                    {
+//                        isDirty = true;
+//                    }
 
-                    if (GUILayout.Button(""))
-                    {
-                    }
-                }
-                EditorGUI.indentLevel--;
-            }
+//                    if (GUILayout.Button(""))
+//                    {
+//                    }
+//                }
+//                EditorGUI.indentLevel--;
+//            }
 
-            EditorHelper.Draw_Separator();
-        }
-        GUILayout.EndVertical();
-    }
+//            EditorHelper.Draw_Separator();
+//        }
+//        GUILayout.EndVertical();
+//    }
 
-    Rect mSaveMenuRect;
-    Rect mViewStateMenuRect;
-    void DrawToolBar()
-    {
-        EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);// EditorStyles.toolbarTextField);
-        {
-            if (GUILayout.Button(New, EditorStyles.toolbarButton, GUILayout.Width(100)))
-            {
-            }
+//    Rect mSaveMenuRect;
+//    Rect mViewStateMenuRect;
+//    void DrawToolBar()
+//    {
+//        EditorGUILayout.BeginHorizontal(EditorStyles.toolbar);// EditorStyles.toolbarTextField);
+//        {
+//            if (GUILayout.Button(New, EditorStyles.toolbarButton, GUILayout.Width(100)))
+//            {
+//            }
 
-            GUILayout.FlexibleSpace();
+//            GUILayout.FlexibleSpace();
 
-            mSaveMenuRect = GUILayoutUtility.GetRect(Save, EditorStyles.toolbarButton, GUILayout.Width(100f));
-            GUI.enabled = isDirty;
-            if (GUI.Button(mSaveMenuRect, Save, EditorStyles.toolbarButton))
-            {
+//            mSaveMenuRect = GUILayoutUtility.GetRect(Save, EditorStyles.toolbarButton, GUILayout.Width(100f));
+//            GUI.enabled = isDirty;
+//            if (GUI.Button(mSaveMenuRect, Save, EditorStyles.toolbarButton))
+//            {
 
-            }
-            GUI.enabled = true;
+//            }
+//            GUI.enabled = true;
 
-            //mViewStateMenuRect = GUILayoutUtility.GetRect(DisplayWindow, EditorStyles.toolbarButton, GUILayout.Width(100f));
-            //if (GUI.Button(mViewStateMenuRect, DisplayWindow, EditorStyles.toolbarButton))
-            //{
+//            //mViewStateMenuRect = GUILayoutUtility.GetRect(DisplayWindow, EditorStyles.toolbarButton, GUILayout.Width(100f));
+//            //if (GUI.Button(mViewStateMenuRect, DisplayWindow, EditorStyles.toolbarButton))
+//            //{
 
-            //}
+//            //}
 
-            //GUILayout.FlexibleSpace();
+//            //GUILayout.FlexibleSpace();
 
-            //bool oldEnabled = GUI.enabled;
-            //GUI.enabled = false;
-            //if (GUILayout.Button(InspectInfo, EditorStyles.toolbarButton))
-            //{
-            //}
-            //GUI.enabled = oldEnabled;
+//            //bool oldEnabled = GUI.enabled;
+//            //GUI.enabled = false;
+//            //if (GUILayout.Button(InspectInfo, EditorStyles.toolbarButton))
+//            //{
+//            //}
+//            //GUI.enabled = oldEnabled;
 
-            //if (GUILayout.Button(ToolSetting, EditorStyles.toolbarButton))
-            //{
-            //}
-        }
-        EditorGUILayout.EndHorizontal();
-    }
-}
+//            //if (GUILayout.Button(ToolSetting, EditorStyles.toolbarButton))
+//            //{
+//            //}
+//        }
+//        EditorGUILayout.EndHorizontal();
+//    }
+//}
